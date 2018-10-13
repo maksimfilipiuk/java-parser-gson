ca3bc5b01032c595bf67f947932b7ace62f7bc2d
Maksim Filipiuk <webidron01@gmail.com>
2018-10-13 17:36:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/**
+ *     Java parser by Maksim Filipiuk (next. Java parser). Using the GSON library.
+ *     Copyright (c) 2018 by Maksim Filipiuk (devMax) <maksim.filipiuk@gmail.com>
+ *
+ *     This file is part of Java parser.
+ *
+ *     This program is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as
+ *     published by the Free Software Foundation; either version 2 of
+ *     the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be
+ *     useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public
+ *     License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import com.google.gson.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Code...
+    }
+}
