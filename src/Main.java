547a72741d1ba1b9546a8f9ad6c7a238c7e11516
Maksim Filipiuk <webidron01@gmail.com>
2018-10-13 18:19:32 +0300

    Update license in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
-/**
- *     Java parser by Maksim Filipiuk (next. Java parser). Using the GSON library.
- *     Copyright (c) 2018 by Maksim Filipiuk (devMax) <maksim.filipiuk@gmail.com>
- *
- *     This file is part of Java parser.
- *
- *     This program is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License as
- *     published by the Free Software Foundation; either version 2 of
- *     the License, or (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be
- *     useful, but WITHOUT ANY WARRANTY; without even the implied
- *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- *     PURPOSE. See the GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public
- *     License along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+      Java parser by Maksim Filipiuk (next. Java parser). Using the GSON library.
+      Copyright (c) 2018 by Maksim Filipiuk (devMax) <maksim.filipiuk@gmail.com>
+
+      This file is part of Java parser.
+
+      Java parser is free software: you can redistribute it and/or
+      modify it under the terms of the GNU General Public License as
+      published by the Free Software Foundation; either version 2 of
+      the License, or (at your option) any later version.
+
+      Java parser is distributed in the hope that it will be
+      useful, but WITHOUT ANY WARRANTY; without even the implied
+      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+      PURPOSE. See the GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public
+      License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import com.google.gson.*;
